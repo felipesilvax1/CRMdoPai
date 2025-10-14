26050e3a6b6517ad45c830b03cf41ab9f0637128
Felipe Miguel <62481594+felipesilvax1@users.noreply.github.com>
2025-10-14 13:07:10 -0300

    fix: Atualizar senha PostgreSQL para 'password'

--- iniciar-migracao-paralela.ps1
+++ iniciar-migracao-paralela.ps1
@@ -67,7 +67,7 @@ if (-not $SkipDump) {
         Write-Host "  Tentando usar Docker..." -ForegroundColor Yellow
         
         # Usar pg_dump do container COM SENHA
-        $dumpCmd = "docker run --rm --network host -e PGPASSWORD=postgres -v ${PWD}:/backup postgres:17 pg_dump -h 192.168.15.22 -U postgres -Fc cnpj_processado -f /backup/cnpj_processado.dump"
+        $dumpCmd = "docker run --rm --network host -e PGPASSWORD=password -v ${PWD}:/backup postgres:17 pg_dump -h 192.168.15.22 -U postgres -Fc cnpj_processado -f /backup/cnpj_processado.dump"
     } else {
         $dumpCmd = "pg_dump -h 192.168.15.22 -U postgres -Fc cnpj_processado -f cnpj_processado.dump"
     }
@@ -89,7 +89,7 @@ Write-Host "========================================" -ForegroundColor Cyan
 Write-Host "`nExecutando dump..." -ForegroundColor Yellow
 
 # Definir senha PostgreSQL
-$env:PGPASSWORD = "postgres"
+$env:PGPASSWORD = "password"
 
 # Executar dump
 '@ + "`n" + $dumpCmd + @'
